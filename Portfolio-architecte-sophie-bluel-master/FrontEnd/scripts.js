ea6765dfd0a0cefe8b5b5c64779659608ba44642
Alexandru DUMITRU <alexandrudumitru@Alexandrus-MBP.lan>
2026-03-30 04:44:20 +0200

    2nd list done

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/scripts.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/scripts.js
@@ -1,10 +1,10 @@
 //**maGallery */
+let allWorks = [];
 const maGallery = document.querySelector(".gallery")
 maGallery.innerHTML = "";
 
-fetch("http://localhost:5678/api/works")
-    .then(reponse => reponse.json())
-    .then(projets => {
+function displayWorks(projets) {
+    maGallery.innerHTML = ""
         projets.forEach(projet => {
             //HTML Création
             const figure = document.createElement("figure");
@@ -17,8 +17,19 @@ fetch("http://localhost:5678/api/works")
             figure.appendChild(image)
             figure.appendChild(figcaption)
         })
-
+    }
+fetch("http://localhost:5678/api/works")
+    .then(reponse => reponse.json())
+    .then(projets => {
+        allWorks = projets;
+        displayWorks(allWorks)
     });
+
+//**function .active */
+function updateActiveBtn(clickedBtn) {
+    document.querySelectorAll(".btn-filtres").forEach(btn => btn.classList.remove("active"));
+    clickedBtn.classList.add("active");
+}
 
 //**mesFiltres */   
 const mesFiltres = document.querySelector(".filtres")
@@ -31,11 +42,20 @@ fetch("http://localhost:5678/api/categories")
             btnAll.innerText = "Tous";
             btnAll.classList.add("btn-filtres");
             mesFiltres.appendChild(btnAll);
+            btnAll.addEventListener("click", () => {updateActiveBtn(btnAll); displayWorks(allWorks)});
 
         category.forEach(category => {
             const btn = document.createElement("button")
             btn.innerText = category.name;
             btn.classList.add("btn-filtres");
             mesFiltres.appendChild(btn);
+
+            btn.addEventListener("click", () => {
+                updateActiveBtn(btn);
+                document.querySelectorAll(".btn-filtres").forEach(b => b.classList.remove("active"));
+                btn.classList.add("active");
+                const filtered = allWorks.filter(work => work.categoryId === category.id);
+                displayWorks(filtered);
+            })
         })    
 });
